4dbbaca9e7b4f2268705ccb834e5133237310b0a
Hamid Ghobakhlo <hamidghobakhlo@gmail.com>
2025-07-07 12:15:25 +0330

    Update main.py

--- 03_colorspaces/code/main.py
+++ 03_colorspaces/code/main.py
@@ -1,14 +1,18 @@
+# Colorspaces
 import os
 
 import cv2
 
-
-img = cv2.imread(os.path.join('.', 'bird.jpg'))
+image_paths= r'C:\Users\...'
+img = cv2.imread(os.path.join(image_paths, 'birds.jpg'))
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_bgr = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 cv2.imshow('img', img)
 cv2.imshow('img_hsv', img_hsv)
+cv2.imshow('img_gray', img_gray)
+cv2.imshow('img_rgb', img_rgb)
+
 cv2.waitKey(0)
